6f0786cf883d665c5ed10729b4231e53d7c17979
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-07-30 14:57:17 +0530

    Create SinglyLinkedList04.java

---
+++ SinglyLinkedList04.java
@@ -0,0 +1,61 @@
+// Node class
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Singly Linked List class
+class SinglyLinkedList {
+    Node head;
+
+    SinglyLinkedList() {
+        this.head = null;
+    }
+
+    // Insert at beginning
+    void insertAtBeginning(int data) {
+        Node nb = new Node(data);
+        nb.next = head;
+        head = nb;
+    }
+
+    // Display the list
+    void display() {
+        if (head == null) {
+            System.out.println("List is Empty");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " --> ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList L = new SinglyLinkedList();
+
+        Node n = new Node(10);
+        L.head = n;
+        Node n1 = new Node(20);
+        n.next = n1;
+        Node n2 = new Node(30);
+        n1.next = n2;
+        Node n3 = new Node(40);
+        n2.next = n3;
+
+        L.display();
+        System.out.println();
+        
+        L.insertAtBeginning(5);
+        L.display();
+    }
+}
