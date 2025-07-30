71567e146a9b1524a637c2968eb4fab5fe76a082
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-07-30 14:54:54 +0530

    Create SinglyLinkedList01.java

---
+++ SinglyLinkedList01.java
@@ -0,0 +1,29 @@
+// Node class
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Singly Linked List class
+class SinglyLinkedList {
+    Node head;
+
+    SinglyLinkedList() {
+        this.head = null;
+    }
+}
+
+// Main class to test the list
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList L = new SinglyLinkedList();
+        Node n = new Node(10);
+        L.head = n;
+        System.out.println(L.head.data);
+    }
+}
