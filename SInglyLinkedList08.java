314debdd33b0e27af41a0f9d4baded5f0a000044
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-07-30 15:02:08 +0530

    Create SInglyLinkedList08.java

---
+++ SInglyLinkedList08.java
@@ -0,0 +1,20 @@
+// Node class
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Singly Linked List class
+class SinglyLinkedList {
+    Node head;
+
+    SinglyLinkedList() {
+        this.head = null;
+    }
+
+    // Display the
